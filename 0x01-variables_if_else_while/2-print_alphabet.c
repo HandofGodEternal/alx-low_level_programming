e5cf78af1556606095807af7ce596aa7f5c41e3e
Victor Ategbeshe <synergyethical@gmail.com>
2022-09-12 18:57:57 -0700

    Different types of script

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - print if the number is postive, zero, or negative
+ *
+ * Description: print the alphabet in lowercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
