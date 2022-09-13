3742b3b36afcd52e31cd0d24d7a59bd5ab8e2674
Victor Ategbeshe <synergyethical@gmail.com>
2022-09-12 19:13:13 -0700

    recheck commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	Lastdigit = n % 10;
+	printf("Last digit of %d is %d and is ", n, Lastdigit);
 	if (Lastdigit > 5)
-	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, Lastdigit);
-	}
+		printf("greater than 5\n");
 	else if (Lastdigit == 0)
-	{
-		printf("Last digit of %d is %d and is 0\n", n, Lastdigit);
-	}
+		printf("0\n");
 	else
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, Lastdigit);
-	}
+		printf("less than 6 and not 0\n");
 	return (0);
 }
