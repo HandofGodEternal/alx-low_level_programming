66eff4d4caabd6eeec0b8884499fdc12aeab4865
Victor Ategbeshe <synergyethical@gmail.com>
2022-09-22 18:19:30 -0700

    more on pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,3 +11,8 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+char *rot13(char *s);
+void print_number(int n);
+int main(void);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+int isPrintableASCII(int n);
